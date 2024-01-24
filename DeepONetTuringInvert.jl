001b873033bdd03c70a8b2482058386a2032d3dd
Ryan Graham <51035941+ryan-d-graham@users.noreply.github.com>
2024-01-23 20:41:25 -0600

    Add files via upload
    Code for training a DeepONet model (neural operator surrogate model) to learn the nonlinear function-space map between randomly-sampled external torques of a nonlinear pendulum and its trajectories. The surrogate model's forward pass is much faster than an ODE solve and therefore can accelerate Bayesian inference of the external torque required to generate an observed (or provided) noisy solution.

--- DeepONetTuringInvert.jl
+++ DeepONetTuringInvert.jl
@@ -2,123 +2,132 @@ using NeuralOperators, Flux, DiffEqFlux
 using DifferentialEquations
 using LinearAlgebra: I
 using Plots, Distributions
-using Integrals, IntegralsCubature
 using Turing, MCMCChains, StatsPlots
 
-# use a nn to approximate an operator 
-xdim = 32 #number of discretized points for input function
-sdim = 1 #dimension of domain of neural operator
-model = DeepONet((xdim, 64, 72), (sdim, 64, 72), σ, tanh) # Learn nonlinear Banach space map between function spaces
+# use a nn to approximate an operator, eg. an ODE/PDE solver, Nonlinear Solver, Optimization problem etc
+# DeepONet approximates a Banach space map using a discretized input function and sensor locations at which to evaluate the operator
+# Architecture: 
+# Model{f(x)}(ξ) = Branch{f(x)}ᵀ Trunk{ξ}, where f(x) is the discretized input function and ξ are the coordinates at which to evaluate operator
+FuncDim = 32 # number of discrete points to approximate input function
+SensorDim = 1 # number of dimensions in input domain of neural operator
+Model = DeepONet((FuncDim, 64, 72), (SensorDim, 64, 72), σ, tanh) # Learn nonlinear Banach space map between function spaces
 
-num_func_obs = 1000 # Number of input functions to use 
-num_sens_obs = 32 # Number of discrete points at which to evaluate input functions
+NumFuncObs = 1024 # Number of randomly-sampled input functions to use for training
+NumSensors = 32 # Number of discrete points at which to evaluate input functions
 
 # create prototype inputs to neural operator model
-xtrain = randn((xdim, num_func_obs))
-sensor = randn((sdim, num_sens_obs))
+FuncTrain = randn((FuncDim, NumFuncObs))
+Sensor = randn((SensorDim, NumSensors))
 # evaluate model at prototype inputs
-model(xtrain, sensor)
+Model(FuncTrain, Sensor)
 
 
 # generate data to train DeepONet using Gaussian Processes, Integrals and Interpolations
-θdim = 4
-σθ_prior = 1.0 # prior standard deviation of parameters
-θ_gt = σθ_prior * randn(θdim) # sample some ground truth parameters
-force(x, θ) = θ' * FourierBasis(length(θ))(x) # use fourier basis functions for external applied torque
-x0 = 0.0 # left endpoint of sensor domain
-xf = 1.0 # right endpoint of sensor domain
-fgrid = range(x0, xf, xdim) # used as grid for nodes in interpolation for integration function
-sgrid = range(x0, xf, num_sens_obs) # used to keep track of integral upper limit/domain of operator
-sensor = reshape(collect(sgrid), (1, num_sens_obs))
-f̂ = [force(x, θ_gt) for x ∈ fgrid] # sanity checks on data generation
-Gfs_ξ = zeros(num_func_obs, num_sens_obs)
-fs = zeros(xdim, num_func_obs)
+θDim = 10 # number of terms in Fourier Basis expansion
+σp = 1.0 # prior standard deviation of parameters
+θGT = σp * randn(θDim) # sample some ground truth parameters
+Force(x, θ) = θ' * FourierBasis(length(θ))(x) # use fourier basis functions for external applied torque
+t₀ = 0.0 # left endpoint of sensor domain
+t₁ = 1.0 # right endpoint of sensor domain
+FunctionGrid = range(t₀, t₁, FuncDim) # used as grid for nodes in interpolation for integration function
+SensorGrid = range(t₀, t₁, NumSensors) # used to keep track of integral upper limit/domain of operator
+Sensor = reshape(collect(SensorGrid), (SensorDim, NumSensors))
+f̂ = [Force(t, θGT) for t ∈ FunctionGrid] # sanity checks on data generation
+SolutionObs = zeros(NumFuncObs, NumSensors)
+FunctionObs = zeros(FuncDim, NumFuncObs)
 #prob = IntegralProblem(force, x0, xf, θ_gt)
 #solve(prob, HCubatureJL())
 
 # x'(t) = v(t)
 # v'(t) = -gsin(x(t)) - cv(t)
 # setup ODE problem
-function odefunc(du, u, p, t)
+function Pendulum!(du, u, p, t)
     du[1] = u[2]
-    du[2] = -9.8 * sin(u[1]) - 0.5 * u[2] + force(t, p)
+    du[2] = -9.8 * sin(u[1]) - 0.5 * u[2] + Force(t, p)
 end
 # initial conditions and external torque function parameters
 u₀ = [0.0, 0.0]
-tspan = (x0, xf)
-p = θ_gt
-prob = ODEProblem(odefunc, u₀, tspan, p)
+tspan = (t₀, t₁)
+prob = ODEProblem(Pendulum!, u₀, tspan, θGT)
 #solve problem
-sol = solve(prob, Tsit5(), saveat = collect(sgrid))
-sol(0.3, idxs=2) # more sanity checks
-plot(sgrid, sol(sgrid, idxs=2), title="GT Solution") # visualize
+sol = solve(prob, Tsit5(), saveat = collect(SensorGrid))
+sol(rand(1)[1], idxs=2) # more sanity checks
+plot(SensorGrid, sol(SensorGrid, idxs=2), title="GT Solution") # visualize angular position without angular velocity (u₂ and not u₁)
 
 # Generate data for DeepONet using u(t) = ODESolve{f(u, t, p)}, t ∈ [x₀, x]
-for ϕ ∈ 1:num_func_obs # iterate over number of functions to sample
-    θ = σθ_prior * randn(θdim) # sample a random function 
-    fs[:, ϕ] = [force(x, θ) for x ∈ fgrid] # put the function in the row of fs
+for ϕ ∈ 1:NumFuncObs # iterate over number of functions to sample
+    θ = σp * randn(θDim) # sample a random function 
+    FunctionObs[:, ϕ] = [Force(t, θ) for t ∈ FunctionGrid] # put the function in the row of fs
     prob = remake(prob, p = θ) # remake ode problem with p = θ
     sol = solve(prob, Tsit5()) # integrate
-    Gfs_ξ[ϕ, :] = sol(sgrid, idxs=2)
+    SolutionObs[ϕ, :] = sol(SensorGrid, idxs=2)
 end
 
-# visualize forcing functions and their forward influence on ODE trajectory (2nd variable only: angular position) 
-fplot = plot(fgrid, fs, alpha = 0.2, legend=false, title="force(t)"); # plot of function samples
-∫fplot = plot(sgrid, Gfs_ξ', alpha = 0.2, legend=false, title="Sol(t)");
-plot(fplot, ∫fplot)
-
 # evaluate model over input functions "fs" at domain coordinates "sensor"
-model(fs, sensor) # another sanity check
+Model(FunctionObs, Sensor) # another sanity check
 
 # Loss function for neural operator network
-loss(xtrain, ytrain, sensor) = Flux.Losses.mse(model(xtrain, sensor), ytrain)
+Loss(fs, sols, sensor) = Flux.Losses.mse(Model(fs, sensor), sols)
 
 # training setup (manually decrease learning rate as training progresses or stalls below desired loss)
 # I like to see a 1.0e-5 or less on final_loss
-learning_rate = 1e-3
-opt = Adam(learning_rate)
-parameters = Flux.params(model)
-Flux.@epochs 1000 Flux.train!(loss, parameters, [(fs, Gfs_ξ, sensor)], opt)
-final_loss = loss(fs, Gfs_ξ, sensor)
+LearningRate = 1e-4
+Opt = Adam(LearningRate)
+Pars = Flux.params(Model)
+Flux.@epochs 1000 Flux.train!(Loss, Pars, [(FunctionObs, SolutionObs, Sensor)], Opt)
+FinalLoss = Loss(FunctionObs, SolutionObs, Sensor)
 
 # Evaluate model accuracy and generalization performance using different lengthscales of fourier basis samples on same model params
-new_res = 10 # enhance resolution by 10x to showcase the discretization-invariance of DeepONet 
-new_sgrid = range(x0, xf, new_res*num_sens_obs)
-new_sensor = reshape(collect(new_sgrid), (1, length(new_sgrid)))
-fplot = plot(fgrid, fs, alpha = 0.5, legend=false, title="force(t)"); # plot forcing functions
-∫fplot = plot(sgrid, Gfs_ξ', alpha = 0.5, legend=false, title="Sol(t)"); # plot true solutions (from solver)
-Gfsplot = plot(new_sgrid, model(fs, new_sensor)', alpha = 0.5, legend=false, title="G{f(x)}(ξ)"); # plot neural surrogate approximations
-plot(fplot, ∫fplot, Gfsplot)
+Ref = 4 # enhance resolution by 10x to showcase the discretization-invariance of DeepONet 
+SensorGridRefined = range(t₀, t₁, Ref*NumSensors)
+NewSensor = reshape(collect(SensorGridRefined), (1, length(SensorGridRefined)))
+# compare ground truth solutions with surrogate model
+p1 = Plots.plot(FunctionGrid, FunctionObs, alpha = 0.3, show=false, legend=false, title="External Forcings"); # plot forcing functions
+p2 = Plots.plot(SensorGrid, SolutionObs', alpha = 0.3, show=false, legend=false, title="True Solutions"); # plot true solutions (from solver)
+p3 = Plots.plot(SensorGridRefined, Model(FunctionObs, NewSensor)', alpha = 0.3, show=false, legend=false, title="Surrogate Solutions"); # plot neural surrogate approximations
+plot(p1, p2, p3)
 
-σl = 1e-2 + final_loss # standard deviation of data likelihood (measurement noise + model final_loss)
+σl = 2e-2 + FinalLoss # standard deviation of data likelihood (measurement noise + model final_loss)
 # setup turing model to infer the parameters of the fourier basis forcing function which generated the noisy solution trace given as data
-@model function BayesFunctionalInverse(Gfs_ξ)
-    θ ~ MultivariateNormal(zeros(θdim), σθ_prior * I(θdim))
-    func = reshape([force(x, θ) for x ∈ fgrid], (xdim, 1))
-    pred = model(func, sensor)
-    for s ∈ 1:num_sens_obs
-        Gfs_ξ[1, s] ~ Normal(pred[1, s], σl)
+@model function BayesFunctionalInverse(SolutionObs)
+    θ ~ MultivariateNormal(zeros(θDim), σp * I(θDim))
+    Func = reshape([Force(t, θ) for t ∈ FunctionGrid], (FuncDim, 1))
+    pred = Model(Func, Sensor)
+    for s ∈ 1:NumSensors
+        SolutionObs[1, s] ~ Normal(pred[1, s], σl)
     end
 end
 
-fselect = 1 # choose a particular solution from the collection generated above
-data = reshape(Gfs_ξ[fselect, :], (1, num_sens_obs)) # the solver data
-data_n = data + σl * randn(size(data)) # data with noise added having amplitude equal to the standard deviation of log likelihood
-funcplot = Plots.plot(fgrid, fs[:, fselect], title="GT Function");
-solplot = Plots.plot(sgrid, data[1, :], title="GT Solution");
-datplot = Plots.plot(sgrid, data_n[1, :], title="Solution Data");
+Select = 2 # choose a particular solution from the collection generated above
+Data = reshape(SolutionObs[Select, :], (1, NumSensors)) # the solver data
+DataNoisy = Data + σl * randn(size(Data)) # data with noise added having amplitude equal to the standard deviation of log likelihood
+funcplot = Plots.plot(FunctionGrid, FunctionObs[:, Select], title="GT Function");
+solplot = Plots.plot(SensorGrid, Data[1, :], title="GT Solution");
+datplot = Plots.plot(SensorGrid, DataNoisy[1, :], title="Solution Data");
 plot(funcplot, solplot, datplot)
 
 # instantiate Turing model with noisy data
-InverseProblem = BayesFunctionalInverse(data_n)
+InverseProblem = BayesFunctionalInverse(DataNoisy)
 
 # sample from model conditional on noisy data using No-U-Turn sampler set at 65% acceptance ratio, 100 samples
-chain = sample(InverseProblem, NUTS(0.65), 100)
-plot(chain) # visualize sampler trajectories and marginal densities
+Iterations = 100
+PosteriorSamples = sample(InverseProblem, NUTS(0.65), Iterations)
+StatsPlots.plot(PosteriorSamples) # visualize sampler trajectories and marginal densities
 
 plot(); # visualize the posterior distribution over forcing functions generating the noisy trajectory data
-for p in eachrow(Array(chain))
-    pred = [force(x, p) for x ∈ fgrid]
-    plot!(fgrid, pred, alpha = 0.05, color=:blue, legend=false)
+for p in eachrow(Array(PosteriorSamples))
+    fpred = [Force(t, p) for t ∈ FunctionGrid]
+    plot!(FunctionGrid, fpred, alpha = 0.05, color=:blue, legend=false)
 end
-plot!(fgrid, fs[:, fselect], lw=1.0, color=:red, title="GT Function(Red); Posterior Traces (Blue)")
+plot!(FunctionGrid, FunctionObs[:, Select], lw=1.0, color=:red, title="GT Function(Red); Posterior Traces (Blue)")
+
+plot(); # visualize the posterior distribution over solutions generating the noisy trajectory data
+for p in eachrow(Array(PosteriorSamples))
+    func = reshape([Force(t, p) for t ∈ FunctionGrid], (FuncDim, 1))
+    prob = remake(prob, p = p)
+    sol = solve(prob, Tsit5())
+    pred = sol(SensorGrid, idxs=2).u
+    plot!(SensorGrid, pred, alpha = 0.05, color=:blue, legend=false)
+    plot!(SensorGrid, Model(func, Sensor)', alpha = 0.05, color=:green, legend=false)
+end
+plot!(SensorGrid, SolutionObs[Select, :], lw=1.0, color=:red, title="GT(Red); Solver(Blue); Surrogate(Green)")
